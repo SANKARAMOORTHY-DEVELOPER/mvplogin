af32b2d5aed3f89fea8404bdfbab3311c7bedbc5
SANKARAMOORTHY-DEVELOPER <78921146+SANKARAMOORTHY-DEVELOPER@users.noreply.github.com>
2022-11-12 19:42:02 +0530

    s

--- components/EmptyContent.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex, Text } from '@chakra-ui/react';
-
-export const EmptyContent = () => {
-  return (
-    <Flex flexDir='column'>
-      <Text color='gray.300'>There is nothing to display.</Text>
-    </Flex>
-  );
-};
